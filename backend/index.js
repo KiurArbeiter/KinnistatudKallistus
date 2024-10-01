d1fe5d5c590774a9c526001aa345ec60eb710c2c
Opilane TTHK <opilane@tthk.ee>
2024-10-01 12:31:41 +0300

    swagger work

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,9 @@
 const app = require('express')()
 const port = 8080
+const swaggerDoc = require("./docs/swagger.json")
+const swaggerUi = require("swagger-ui-express")
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc))
 
 app.listen(port, () => {
     console.log(`API up at : http:/localhost:${port}`)
