7f24403d96d38df5bbf8de32d24218c879dc177e
Opilane TTHK <opilane@tthk.ee>
2024-10-03 13:03:59 +0300

    ü

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
 const app = require('express')()
+const host = 'localhost'
 const port = 8080
 const swaggerDoc = require("./docs/swagger.json")
 const swaggerUi = require("swagger-ui-express")
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc))
 
-app.get("/games", (req, res) => {
-    res.send(["Witcher 3", "Cyberpunk 2077"])
+app.get("/", (req, res) => {
+    res.send(`Server running. Docs at <a href='http://${host}:${port}/docs'> /docs</>`)
+})
+
+app.get("/hugs", (req, res) => {
+    res.send(["short", "long"])
 })
 
 
