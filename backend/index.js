bb5433c8fbaa683a858d209f2c5b5ba79548429b
Opilane TTHK <opilane@tthk.ee>
2024-11-14 10:06:32 +0200

    added missing files

--- backend/index.js
+++ backend/index.js
@@ -1,62 +1,26 @@
-const app = require('express')()
+require('dotenv').config();
+const port = process.env.PORT || 3001;
 const host = 'localhost'
-const port = 8080
-const swaggerDoc = require("./docs/swagger.json")
+const express = require("express")
+const cors = require("cors")
+const app = express()
 const swaggerUi = require("swagger-ui-express")
 
-const hugs = [
-    {id: 1, name: "Short", price: 20.50},
-    {id: 2, name: "Medium", price: 50.20},
-    {id: 3, name: "Long", price: 70.00}
-]
+const swaggerDoc = require("./docs/swagger.json")
+const {sync} = require("./db");
 
+app.use(cors());
+app.use(express.json())
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc))
 
 app.get("/", (req, res) => {
-    res.send(`Server running. Docs at <a href='http://${host}:${port}/docs'> /docs</a>`)
+    res.send(`Server running. Docs at <a href="http://${host}:${port}/docs">/docs</a>`)
 })
 
-app.get("/hugs", (req, res) => {
-    res.send(hugs.map(({id,name}) => {
-         return {id, name}
-    }))
-})
+require("./routes/hugRoutes")(app)
+require("./routes/clientRoutes")(app)
 
-function createId() {
-    const maxIdHug = hugs.reduce((prev, current) => (prev.id > current.id) ? prev : current, 1)
-    return maxIdHug.id + 1;
-}
-
-
-app.post("/hugs", (req, res) => {
-    if(!req.body.name || req.body.name.trim().lenght === 0) {
-        return res.status(400).send({error: "Missing required field 'name'"})
-    }
-    const newPrice = parseFloat(req.body.price);
-    hugs.push({
-            id: createId(),
-            name: req.body.name,
-            price: isNaN(newPrice) ? null : newPrice
-        }
-    )
-    res.send(hugs)
-})
-
-
-app.get("/hugs/:id", (req, res) => {
-    const idNumber = parseInt(req.params.id)
-    if (isNaN(idNumber)) {
-        return res.status(400).send({error: `ID must be a whole number: ${req.params.id}`})
-    }
-    const hug = hugs.find(g => g.id === idNumber)
-    if (!hug) {
-        return res.status(404).send({error: `Hug Not Found!`})
-    }
-    res.send(hug)
-})
-
-
-
-app.listen(port, () => {
-    console.log(`API up at : http:/localhost:${port}`)
+app.listen(port, async () => {
+    if (process.env.SYNC === "true") { await sync(); }
+    console.log(`API up at: http://${host}:${port}`)
 })
