7467a687062089252285293f05f384a94c903e3e
Opilane TTHK <opilane@tthk.ee>
2024-11-29 10:22:50 +0200

    client crud?

--- backend/models/Client.js
+++ backend/models/Client.js
@@ -12,6 +12,10 @@ module.exports = (sequelize, DataTypes) => {
                 type: DataTypes.STRING,
                 allowNull: false,
             },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
         },
         {
             // Other model options go here
